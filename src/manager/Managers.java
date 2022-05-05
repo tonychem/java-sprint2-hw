9b70c6c9a3c2b4cd46926d34c0da44899d6521ae
Anatoly <tonychem@yandex.ru>
2022-05-05 20:44:14 +0300

    feat: добавить getFileBackedTasksManager в интерфейс Managers

--- src/manager/Managers.java
+++ src/manager/Managers.java
@@ -1,8 +1,12 @@
 package manager;
 
 public class Managers {
-    public static TaskManager getDefault() {
+    public static TaskManager getInMemoryTaskManager() {
         return new InMemoryTaskManager();
+    }
+
+    public static TaskManager getFileBackedTasksManager(String path) {
+        return new FileBackedTasksManager(path);
     }
 
     public static HistoryManager getDefaultHistory() {
